b0bbf5bc8f1cb285e247f0f6bba154eacaadb3f8
Omar Jabour <jabour.omar@hotmail.com>
2026-07-19 19:55:16 +0200

    Methods for String: ././././././   :art:

--- hello.py
+++ hello.py
@@ -1,21 +1,2 @@
-acc=46898764356
-id=1256
-name="root"
-print("Account = ", acc, "Id = ", id, name)
-print(name)
-print(name, id)
-
-# A variable can store sata of different types in Python.
-
-""" 
-Data exists in many forms:
-- 45 is an integer data type
-- 23.5 is a float data type
-- "root" is a string data type
-"""
-
-# Variables
-a = 78
-name = "test"
-salary= 3456.67
-print(type(name))
+# Methods for String
+name= "root admin user"
