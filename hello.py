811ee0af5808faabf816512fc4b7904af329b97f
Omar Jabour <jabour.omar@hotmail.com>
2026-07-28 00:41:19 +0200

    Methods Strings: String-Formating  /././././   :art:

--- hello.py
+++ hello.py
@@ -43,4 +43,4 @@ apeydo = "admin"
 id = 105
 print(f"My name is {apeydo} and my id is {id}")
 print(f"My name is {apeydo} and  {34*34}")
-print(f"My name is {apeydo} and  {34/34}")
+print(f"My name is {apeydo} and  {34/46:.2f}") #
