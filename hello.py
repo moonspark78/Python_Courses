c9a602fe4cc01e56aa90815d63ae7dcc949587dd
Omar Jabour <jabour.omar@hotmail.com>
2026-07-27 20:35:02 +0200

    Methods Strings: Sub-Strings /./././  :art:

--- hello.py
+++ hello.py
@@ -25,3 +25,7 @@ print(url[8:-4]) # google
 #-----------------Count how many time a sub-strings---------------------------
 name1 = "root user root admin"
 print(name1.count("root"))
+
+#-----------------Len()----------------------
+name2= " root user root admin root "
+print("the len of the strings before applying stripfunc", len(name2))
