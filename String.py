1e562b4b56005ec25b07192ad03572b941521edd
Omar Jabour <jabour.omar@hotmail.com>
2026-08-02 15:10:30 +0200

    String concatenation.  :art:

--- String.py
+++ String.py
@@ -6,4 +6,4 @@ print("root" + " " + "user") #rootuser
 print("-----------")
 fname = "root"
 lname = "user"
-print("The fullname is  ," + fname + " " + lname) #root user
+print("The fullname is ," + fname + " " + lname) #root user
