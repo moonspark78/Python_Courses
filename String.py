1d445b9b1cd4035c479ce5233328e18f490aeeb1
Omar Jabour <jabour.omar@hotmail.com>
2026-08-03 22:02:34 +0200

    String concatenation  ././././././   :art:

--- String.py
+++ String.py
@@ -5,7 +5,8 @@ print("root" + " " + "user") #rootuser
 
 print("-----------")
 fname = "root"
-f1name = "root"
+f1name = "root1"
 lname = "user"
+print("The fullname is ," + f1name + " " + lname) #root1 user
 print("The fullname is ," + fname + " " + lname) #root user
 print(12 + "root") #TypeError: unsupported operand type(s) for +: 'int' and 'str'
