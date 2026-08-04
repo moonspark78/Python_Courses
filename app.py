d2734a97b9c95a9196e7ae5816bab4f06d4e0eee
Omar Jabour <jabour.omar@hotmail.com>
2026-08-04 20:57:05 +0200

    Mini project calculator  :art:

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ a = int(input("Enter a number: "))
 b = int(input("Enter a number: "))
 
 sum = a + b
+multiplication = a * b
 
 print("The sum of two numbers is: ", sum)
 print("The difference of two numbers is: ", a - b)
-print("The product of two numbers is: ", a * b)
+print("The product of two numbers is: ", multiplication)
 print("The division of two numbers is: ", a / b)
 print("The modulus of two numbers is: ", a % b)
