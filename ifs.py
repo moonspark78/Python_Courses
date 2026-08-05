49ee47419e96e685a1167c6c408ac9b064584c76
Omar Jabour <jabour.omar@hotmail.com>
2026-08-05 13:14:00 +0200

    :bento: If Statement:  /./././.   :art:

--- ifs.py
+++ ifs.py
@@ -3,7 +3,12 @@ if 45 > 10:
     print("45 is greater than 10")
     print("This is a simple if statement")
     print("True")
-    
+else:
+    print("This is a simple else statement")
+    print("False")
+
+
+
     
 if 45 > 100:
     print("45 is greater than 10") #This will not be printed because the condition is false
