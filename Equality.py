b1dfe8ff046caf1ef8f7eb744add7440842e145f
Omar Jabour <jabour.omar@hotmail.com>
2026-08-05 16:49:11 +0200

    :bento: If Statement: .  :art:

--- Equality.py
+++ Equality.py
@@ -18,3 +18,17 @@ if 12!=8:
     print("The numbers are not equal.")
 else:
     print("The numbers are equal.")
+    
+#----------Logical Operators----------#
+#AND OR NOT
+
+if 22==22 and 1==1 and 3==3:
+    print("Both conditions are true.")
+else:
+    print("One or both conditions are false.")
+    
+
+if 22==22 or 1==2 or 3==4:
+    print("At least one condition is true.")
+else:
+    print("All conditions are false.")
