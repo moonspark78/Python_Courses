44486afb4f7f0d37f15898e1ac93eca55d401807
Omar Jabour <jabour.omar@hotmail.com>
2026-08-05 16:40:17 +0200

    :bento: If Statement:  :art:

--- Equality.py
+++ Equality.py
@@ -6,3 +6,9 @@ if 34==344:
     print("The numbers are equal.")
 else:
     print("The numbers are not equal.")
+    
+    
+if "root"=="root ":
+    print("The strings are equal.")
+else:
+    print("The strings are not equal.")
