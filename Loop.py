6d5ac80d3cad520f6a75e337c2def076aa48651b
Omar Jabour <jabour.omar@hotmail.com>
2026-08-07 13:52:43 +0200

    :bento: Loop  /././././././   :art:

--- Loop.py
+++ Loop.py
@@ -7,3 +7,9 @@ for i in range (1,12,1):
 for i in range (1,12,1):
     print("hello world", i)
     print("For loop is running")
+    
+    
+#----------More Fore Loop---------
+# Dans le in range 12-1 
+for i in range (1, 12): 
+    print(i)
