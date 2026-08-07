4d8081c3d50d2e571484a2ce4c62be3db1eb3867
Omar Jabour <jabour.omar@hotmail.com>
2026-08-07 13:53:12 +0200

    :bento: Loop  /./././././.   :art:

--- Loop.py
+++ Loop.py
@@ -11,5 +11,5 @@ for i in range (1,12,1):
     
 #----------More Fore Loop---------
 # Dans le in range 12-1 
-for i in range (1, 12): 
+for i in range (1, 12): #On précise le nombre de pas c'est par défauts 1
     print(i)
