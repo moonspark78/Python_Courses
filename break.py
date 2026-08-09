7ce6e82059381395d766547adad922484d188af9
Omar Jabour <jabour.omar@hotmail.com>
2026-08-09 15:56:39 +0200

    :bento: Beak   /././././././../  :art:

--- break.py
+++ break.py
@@ -1,3 +1,5 @@
 #Break statement is used to exit a loop when a certain condition is met. It can be used in both for and while loops.
 for i in range(1, 10+1):
     print(i)
+    if i == 5:
+        break
