a0f1a390b835a8f9b8a0645ab44941fd550bf6a2
Omar Jabour <jabour.omar@hotmail.com>
2026-08-18 19:00:32 +0200

    :package: Python: Return .              :card_file_box:

---
+++ return.py
@@ -0,0 +1,5 @@
+""" 
+Returns a value from a function. This is used to exit a function and optionally
+pass an expression back to the caller. The return statement can be used with or without an expression.
+If no expression is provided, the function will return None by default.
+"""
