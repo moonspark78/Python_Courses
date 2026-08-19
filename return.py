f7c9c1693873cecf38d67e603bf0612703ab92e3
Omar Jabour <jabour.omar@hotmail.com>
2026-08-19 22:44:56 +0200

    :package: Python: Return ././././              :card_file_box:

--- return.py
+++ return.py
@@ -3,3 +3,7 @@ Returns a value from a function. This is used to exit a function and optionally
 pass an expression back to the caller. The return statement can be used with or without an expression.
 If no expression is provided, the function will return None by default.
 """
+
+def add():
+    retrun 5 + 3
+    
