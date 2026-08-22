e6dd701c269e4adc679f577c7ad85d7792ffc46f
Omar Jabour <jabour.omar@hotmail.com>
2026-08-22 16:40:01 +0200

    :package: Python: Project cal /   :card_file_box:

--- ProjectCal.py
+++ ProjectCal.py
@@ -19,7 +19,8 @@ def sub(a, b):
     print("The difference is: ", a - b)
     
 def div(a, b):
-    
+    if b == 0:
+        print("Error: Division by zero is not allowed.")
 
 if choice == "1":
     add(num1, num2)
