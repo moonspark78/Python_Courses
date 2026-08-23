983c73d4211f69bc5291f5c16389fae7af59a3a3
Omar Jabour <jabour.omar@hotmail.com>
2026-08-23 14:41:50 +0200

    :package: Python: Project cal   ./././././.   :card_file_box:

---
+++ Lists.py
@@ -0,0 +1,7 @@
+""" 
+List is a special data structure in Python that allows you to store multiple items in a single variable.
+Lists are ordered, changeable, and allow duplicate values. They are defined by enclosing elements in square brackets [].
+"""
+
+a= [1, 2, 3, 4, 5]  # A list of integers
+print(a)  # Output: [1, 2, 3, 4, 5]
