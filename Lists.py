fdedf0dff10323453f6fd8e6377d50de304b20dc
Omar Jabour <jabour.omar@hotmail.com>
2026-08-23 16:22:15 +0200

    :package: Python: Project cal   ././././   :card_file_box:

--- Lists.py
+++ Lists.py
@@ -8,3 +8,5 @@ print(a)  # Output: [1, 2, 3, 4, 5]
 print(type(a))  # Output: <class 'list'>
 
 print(a[0])  # Output: 1 (Accessing the first element)
+a[0] = 10  # Modifying the first element
+print(a)  # Output: [10, 2, 3, 4,
