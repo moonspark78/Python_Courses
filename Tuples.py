04ebf439dc8f28c31bd2d0a9978319c100d438d3
Omar Jabour <jabour.omar@hotmail.com>
2026-08-28 20:49:09 +0200

    :package: Python: ListMethod.  :card_file_box:

---
+++ Tuples.py
@@ -0,0 +1,5 @@
+""" 
+Tuples are immutable sequences, typically used to store collections of heterogeneous data.
+They are similar to lists, but unlike lists, they cannot be changed after their creation.
+Tuples are defined by enclosing the elements in parentheses `()`, and they can contain any number of elements, including zero.
+"""
