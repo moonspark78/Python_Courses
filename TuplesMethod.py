66ebab47b504405b6ebfc77bd5ddfe122ed8f7a0
Omar Jabour <jabour.omar@hotmail.com>
2026-09-01 22:06:36 +0200

    :package: deposite  ./   :card_file_box:

--- TuplesMethod.py
+++ TuplesMethod.py
@@ -4,3 +4,5 @@ a =list(a)  # Converting the tuple to a list
 
 a.insert(2, 100)  # Inserting the value 100 at index 2
 print(a)  # Output: [1, 2, 100, 4, 57, 87, 53, 239, 8, 52]
+
+print(len(a))  # Output: 10, the length of the list after insertion
