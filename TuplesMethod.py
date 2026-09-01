04788fd3b7ee06667407cd326c3ef9e674e31823
Omar Jabour <jabour.omar@hotmail.com>
2026-09-01 22:03:00 +0200

    :package: deposite  ././   :card_file_box:

--- TuplesMethod.py
+++ TuplesMethod.py
@@ -2,5 +2,4 @@ a=(1,2,4,57,87,53,239,8,52)
 print(type(a))  # Output: <class 'tuple'>
 a =list(a)  # Converting the tuple to a list
 
-print(a)  # Output: [1, 2, 4, 57, 87, 53, 239, 8, 52]
-print(type(a))  # Output: <class 'list'>
+a.insert(2, 100)  # Inserting the value 100 at index 2
