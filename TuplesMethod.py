cb07755a18c59de924fee08ab1917ae650296316
Omar Jabour <jabour.omar@hotmail.com>
2026-09-01 22:13:55 +0200

    :package: deposite  .   :card_file_box:

--- TuplesMethod.py
+++ TuplesMethod.py
@@ -6,3 +6,7 @@ a.insert(2, 100)  # Inserting the value 100 at index 2
 print(a)  # Output: [1, 2, 100, 4, 57, 87, 53, 239, 8, 52]
 
 print(len(a))  # Output: 10, the length of the list after insertion
+
+
+print("---------------------------")
+user={"name": "John", "age": 30, "city": "New York"}
