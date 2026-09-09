f97e42a372a7a4a1a74fbb29844b362259a79bbc
Omar Jabour <jabour.omar@hotmail.com>
2026-09-09 22:30:43 +0200

    :package:  ././././   :card_file_box:

---
+++ Set.py
@@ -0,0 +1,5 @@
+""" 
+A set is an unordered collection of unique elements.
+It is a built-in data type in Python that allows you to store multiple items in a single variable. Sets are mutable,
+meaning you can add or remove elements from them, but they do not allow duplicate values.
+"""
