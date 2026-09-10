89c1aee52ea45ed8997e9878bf0fb5db21cc3eb5
Omar Jabour <jabour.omar@hotmail.com>
2026-09-10 22:29:19 +0200

    :package: MethodSet :  ././././   :card_file_box:

--- SetMethod.py
+++ SetMethod.py
@@ -1,3 +1,5 @@
 a = {12,4,56,232,8,777,1}
 a.add(99)  # Adding an element to the set
 print(a) 
+""" il ajoute l'élément 99 à l'ensemble a.
+L'ordre des éléments dans l'ensemble peut varier car les ensembles sont non ordonnés. """
