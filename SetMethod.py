55aca7d39cd38cb46b11ba0c8938c2edae8b802a
Omar Jabour <jabour.omar@hotmail.com>
2026-09-11 21:07:17 +0200

    :package: MethodSet :  ./././.   :card_file_box:

--- SetMethod.py
+++ SetMethod.py
@@ -21,3 +21,5 @@ print(a)  # Output: set(), the set is now empty
 b = {11, 3, 5, 7, 9}
 b.remove(5)  # Removing an element from the set
 print(b)  # Output: {11, 3, 7, 9},
+
+b.remove(15)  # Attempting to remove an element that does not exist in the set
