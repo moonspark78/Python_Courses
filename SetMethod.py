06ef66bfa0b853bb084055ce8c7132bec0856d7b
Omar Jabour <jabour.omar@hotmail.com>
2026-09-11 21:08:30 +0200

    :package: MethodSet :  ././.   :card_file_box:

--- SetMethod.py
+++ SetMethod.py
@@ -24,3 +24,5 @@ print(b)  # Output: {11, 3, 7, 9},
 
 b.remove(15)  # Attempting to remove an element that does not exist in the set
 print(b)  # Output: {11, 3, 7, 9}, the set remains unchanged
+
+# discard() method can be used to remove an element from the set without raising an error if the element does not exist. For example:
