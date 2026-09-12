7243267b3a22a892805bcc15ada286c3bcbfbb48
Omar Jabour <jabour.omar@hotmail.com>
2026-09-12 19:28:49 +0200

    :package: MethodSet : .  :card_file_box:

--- SplitJoin.py
+++ SplitJoin.py
@@ -5,3 +5,5 @@ print(x)  # Output: ['Root', 'admin', 'user']
 nemes = ["Root", "admin", "user"]
 full_name = " ".join(nemes)
 print(full_name)  # Output: Root admin user
+
+full_name = ",".join(nemes)
