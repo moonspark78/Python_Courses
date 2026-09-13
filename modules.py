c5f50043f79a1f01729ea909d65cab5fc971b4df
Omar Jabour <jabour.omar@hotmail.com>
2026-09-13 15:14:06 +0200

    :package: MethodSet : /./././   :card_file_box:

--- modules.py
+++ modules.py
@@ -6,3 +6,4 @@ The split() method is used to split a string into a list of substrings based on 
 import math
 
 print(math.sqrt(16))  # Output: 4.0, the square root of 16
+print(math.pow(2, 3))  # Output: 8.0, 2 raised to the power of 3
