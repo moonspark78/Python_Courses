0a6757476b1ca320df5f61c2984c75e6ca9ef949
Omar Jabour <jabour.omar@hotmail.com>
2026-09-13 15:13:42 +0200

    :package: MethodSet : /./././.   :card_file_box:

--- modules.py
+++ modules.py
@@ -2,3 +2,7 @@
 Module for demonstrating the use of split() and join() methods in Python.
 The split() method is used to split a string into a list of substrings based on a
 """
+
+import math
+
+print(math.sqrt(16))  # Output: 4.0, the square root of 16
