e2e84bf744efd4fd5638fb86068021a13432dafa
Omar Jabour <jabour.omar@hotmail.com>
2026-09-15 20:51:42 +0200

    :package: DateTime :   /./././   :card_file_box:

--- dateTime.py
+++ dateTime.py
@@ -4,7 +4,8 @@ x=datetime.datetime.now()
 print(x)  # Output: Current date and time in the format YYYY-MM-DD HH:
 
 
-t=datetime.datetime.now()
+t=datetime.datetime.now().year
+print(t)  # Output: Current date and time in the format YYYY-MM-DD HH:MM:SS.mmmmmm
 
 
 
