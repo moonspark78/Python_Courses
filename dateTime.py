2888ed2c5574179d0a7c632045c38222f8c3a188
Omar Jabour <jabour.omar@hotmail.com>
2026-09-16 18:12:09 +0200

    :package: DateTime :   /./.   :card_file_box:

--- dateTime.py
+++ dateTime.py
@@ -11,3 +11,12 @@ print(t)  # Output: Current date and time in the format YYYY-MM-DD HH:MM:SS.mmmm
 month=datetime.datetime.now().month
 print(month)  # Output: Current month as an integer (1-12)
 
+
+# Format code :
+# %A -> Full weekday name
+# %B -> Full month name
+# %d -> Day of the month as a zero-padded decimal number (01-31)
+# %Y -> Year with century as a decimal number
+# %H -> Hour (24-hour clock) as a zero-padded decimal number (00-23)
+# %M -> Minute as a zero-padded decimal number (00-59)
+# %S -> Second as a zero-padded decimal number (00-59)
