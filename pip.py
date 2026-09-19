e6f5040e6888c30f7e1bce7759796a8c18772b5a
Omar Jabour <jabour.omar@hotmail.com>
2026-09-19 20:06:11 +0200

    :package:  Pip:   /././././.   :card_file_box:

--- pip.py
+++ pip.py
@@ -3,3 +3,5 @@ pip is a package management system used to install and manage software packages 
 It is the most widely used tool for installing Python packages from the Python Package Index (PyPI) and other package repositories.
 Pip allows users to easily install, upgrade, and remove Python packages, as well as manage dependencies between packages.
 """
+
+#pip 3 is used to install packages for Python 3.x versions, while pip 2 is used for Python 2.x versions.
