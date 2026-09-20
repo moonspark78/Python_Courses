fa12aa5301bd5fb9525788ea1637e1310ed2bb44
Omar Jabour <jabour.omar@hotmail.com>
2026-09-20 13:53:59 +0200

    :package:  ReadFile:   :card_file_box:

--- ReadFile.py
+++ ReadFile.py
@@ -2,3 +2,6 @@ file=open("demo.txt","r")
 print(file)
 print(file.read())
 file.close()
+
+print("------------------------------------")
+print(file.read(9)) # This will raise an error because the file is closed
