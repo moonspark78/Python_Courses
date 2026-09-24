8aca1c79f42528f097423727779df72b2a922111
Omar Jabour <jabour.omar@hotmail.com>
2026-09-24 20:49:01 +0200

    :package: ./  :card_file_box:

--- main.py
+++ main.py
@@ -10,3 +10,10 @@ class User(BaseModel):
     username: str
     email: str
     age: int
+    
+    
+app = FastAPI()
+
+@app.post("/register")
+async def register_user(user: User):
+    return {"message": f"User {user.username} registered successfully!"}
