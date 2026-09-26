8ba50813bc6688122c704b922ecdc417d1f91a6a
Omar Jabour <jabour.omar@hotmail.com>
2026-09-26 21:59:53 +0200

    :package: Python: ErrorHanfling  ././   :card_file_box:

--- ErrorHandling.py
+++ ErrorHandling.py
@@ -17,3 +17,8 @@ except:
     
 print("the execution continues after the error handling")
 print(12/0) # This line will raise a ZeroDivisionError and terminate the program since it is not within a try-except block.
+
+
+print("----------------------------------------------------------   ")
+try:
+    print(12/0)
