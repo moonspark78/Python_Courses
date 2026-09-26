ad77a59a574f2543cc32b590fa13cf5de8f3e420
Omar Jabour <jabour.omar@hotmail.com>
2026-09-26 21:37:07 +0200

    :package: Python: ErrorHanfling  :card_file_box:

---
+++ ErrorHandling.py
@@ -0,0 +1,7 @@
+""" 
+The are 2 major types of errors in Python:
+1. Syntax Errors: These occur when the Python parser encounters a syntactically incorrect statement.
+2. Runtime Errors: These occur during the execution of a program, after it has passed the syntax check.
+They can be caused by various issues such as division by zero, accessing a non-existent index in a list,
+or calling a function with incorrect arguments.
+"""
